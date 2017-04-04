773c423a0dd5aeb8a26541d29960a7d11c1cf32c
Nitesh Jatav <nitesh.jatav@synapse.com>
2017-04-04 12:17:09 +0530

    git add routing for table

--- src/app/app-routing.module.js
+++ src/app/app-routing.module.js
@@ -10,10 +10,12 @@ var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var login_component_1 = require("./login.component");
 var home_component_1 = require("./home.component");
+var table_component_1 = require("./table.component");
 var routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: login_component_1.LoginComponent },
-    { path: 'home', component: home_component_1.HomeComponent }
+    { path: 'home', component: home_component_1.HomeComponent },
+    { path: 'table', component: table_component_1.TableComponent }
 ];
 var AppRoutingModule = (function () {
     function AppRoutingModule() {
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent }     from './login.component';
 import { HomeComponent }      from './home.component';
+import { TableComponent } from './table.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login',  component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'table', component: TableComponent }
 ];
 
 @NgModule({
